2e5ca68a3ab3583cd8e7c462500353012ed340b0
Kishan Lamba <kishanlamba59@gmail.com>
2024-11-23 21:00:07 +0530

    run build solved by dynamic import of react-lottie and ignoring eslint rules in next.config.mjs file

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -3,12 +3,14 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./BackgroundGradientAnimation";
 import { GlobeDemo } from './GridGlobe';
-import Lottie from "react-lottie";
 import { useState } from "react";
 import animationData from '@/data/confetti.json';
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
 import { authorInfo } from "@/data";
+
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 export const BentoGrid = ({
   className,
@@ -56,7 +58,7 @@ export const BentoGridItem = ({
     setCopied(true);
   }
 
-  const techStatck = ['React', 'NextJs', 'Typescript', 'Express', 'MongoDB', 'NodeJs']
+  const techStatck = ['React', 'NextJs', 'Typescript', 'Express', 'MongoDB', 'NodeJs'];
   
   return (
     <div
