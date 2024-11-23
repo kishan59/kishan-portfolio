4c66fa2de501257b0fd53ee43a4107941611a269
Kishan Lamba <kishanlamba59@gmail.com>
2024-11-23 21:39:37 +0530

    reverting everything back to normal, *do not use github pages for nextjs because image problem. have to use absolute path for <img /> tags. Not worth it!

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true
     },
-    reactStrictMode: true,
-    images: {
-        unoptimized: true, // Disable default image optimization
-    },
-    basePath: '/kishan-portfolio',     // this is only for github pages deployment, remove it when deploy to others
-    assetPrefix: '/kishan-portfolio/',     // this is only for github pages deployment, remove it when deploy to others
 };
 
 export default nextConfig;
